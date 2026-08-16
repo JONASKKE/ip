dd4c9b0840128ef34638dad8301047ffedd9c198
JONASKKE <jokoh6699@gmail.com>
2026-08-16 18:23:16 +0800

    Complete Level 0 : Rename, Greet, Exit

---
+++ src/main/java/Nano.java
@@ -0,0 +1,13 @@
+public class Nano {
+    public static void main(String[] args) {
+        String banner = "NN   NN   AAA   NN   NN   OOO \n"
+                + "NNN  NN  AA AA  NNN  NN  OO OO\n"
+                + "NN N NN AA   AA NN N NN OO  OO\n"
+                + "NN  NNN AAAAAAA NN  NNN OO  OO\n"
+                + "NN   NN AA   AA NN   NN  OOO \n";
+        System.out.println(banner);
+        System.out.println("Hello! I'm Nano.");
+        System.out.println("How may i assist you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
